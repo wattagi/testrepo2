5d851965219321ab44c20e64e7b2f20963da52c3
wattagi <agimelton@gmail.com>
2024-08-08 16:05:57 -0400

    pushing TotalLength Prac Task

--- speedAnalysis/speed_analysis.js
+++ speedAnalysis/speed_analysis.js
@@ -24,6 +24,7 @@ button.onclick = endTest;
             // Calculate time elapsed and words per minute (WPM)
             var timeElapsed = (endTime - startTime) / 1000; // in seconds
             var userTypedText = document.getElementById("userInput").value;
+            //var userTypedText = document.getElementById("userInput").length;
 
             // Split the text using regex to count words correctly
             var typedWords = userTypedText.split(/\s+/).filter(function (word) {
@@ -39,6 +40,7 @@ button.onclick = endTest;
             // Display the results
             var outputDiv = document.getElementById("output");
             outputDiv.innerHTML = "<h2>Typing Test Results:</h2>" +
+            "<p>Total Length: " + userTypedText.length + "</p>" +
                 "<p>Words Typed: " + typedWords + "</p>" +
                 "<p>Time Elapsed: " + timeElapsed.toFixed(2) + " seconds</p>" +
                 "<p>Words Per Minute (WPM): " + wpm + "</p>";
